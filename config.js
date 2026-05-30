a46b4c06ddf2633ec12fb8a243361087433e6667
mjameson1016 <hello@mattjameson.co>
2026-05-30 01:09:39 -0500

    Update config.js

--- config.js
+++ config.js
@@ -84,7 +84,7 @@ I would love to chat with you about working together: hello@mattjameson.co`,
   contact: {
     heading: "Get in touch",
     email:   "hello@mattjameson.co",
-    message: "If you would like to discuss an idea or a project, or you just simply want to say hello, feel free to contact me.",
+    message: "If you would like to discuss an idea or a project, or you just want to say hello, feel free to reach out.",
   },
 
   // ----------------------------------------------------------
